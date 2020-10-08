49f234a9e4fa1317105efb9548f6e420ae6da88e
yefei <33936453@qq.com>
2020-10-08 17:18:59 +0800

    上传李老师修改的代码

--- code/server/center/CenterServerLogic/Client/DataClient.cpp
+++ code/server/center/CenterServerLogic/Client/DataClient.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include <math.h>
 #include <time.h>
 #include "DataClient.h"
 #include "ICenterServer.h"
@@ -68,25 +69,25 @@ void CDataClient::ProcessNetPack()
 //=====================================================
 // 功能：接收到传感器数据的处理
 //
-void CDataClient::RecvAddSensorData(const void *pPack, const unsigned int uPackLen)
+void CDataClient::RecvAddSensorData(const void *pPack, const unsigned int uPackLen)//pPack=集中器上传数据包（协议包），uPackLen=pPack的包长
 {
-	SProtocolHead	*pInfo			= (SProtocolHead*)pPack;
-	SSensorHead		*pSensorHead	= (SSensorHead*)((char*)pPack + sizeof(SProtocolHead));
+	SProtocolHead	*pInfo			= (SProtocolHead*)pPack;	//pInfo<-pPack
+	SSensorHead		*pSensorHead	= (SSensorHead*)((char*)pPack + sizeof(SProtocolHead));//pSensorHead=第一个传感器包头，在协议包包头之后。
 
-	if (0x7E81 != pInfo->wProtocolHead)
+	if (0x7E81 != pInfo->wProtocolHead)//如果找不到协议包头，说明pInfo不是协议包，记录log，然后退出
 	{
-		g_pFileLog->WriteLog("[%s][%d] Protocol Not 7E81\n", __FILE__, __LINE__);
+		g_pFileLog->WriteLog("[%s][%d] Protocol Not 7E81\n", __FILE__, __LINE__);//记录日志
 		return;
 	}
-
-	if (0 == pInfo->bySensorCount)
+  //到此可知pInfo是协议包
+	if (0 == pInfo->bySensorCount)	//查看协议包中传感器数量，=0则说明是心跳包
 	{
-		m_pClientConn->PutPack(pPack, uPackLen);
+		m_pClientConn->PutPack(pPack, uPackLen);//什么意思？将心跳包发回给集中器！
 		g_pFileLog->WriteLog("[%s][%d] Sensor Count Is 0\n", __FILE__, __LINE__);
 		return;
 	}
-
-	IMysqlQuery	*pMysqlQuery = g_ICenterServer.GetMysqlQuery();
+//到此可知pInfo是协议包，且是非心跳包(即传感器数据包）
+	IMysqlQuery	*pMysqlQuery = g_ICenterServer.GetMysqlQuery();//建立一个查询
 	if (nullptr == pMysqlQuery)
 		return;
 
@@ -97,32 +98,33 @@ void CDataClient::RecvAddSensorData(const void *pPack, const unsigned int uPackL
 	double	dValue4			= 0.0;
 	UINT	uSlopeIndex		= 0;
 	UINT	uSensorIndex	= 0;
+	BYTE	byType			= 0;
 
-	for (auto nIndex = 0; nIndex < pInfo->bySensorCount; ++nIndex)
+	for (auto nIndex = 0; nIndex < pInfo->bySensorCount; ++nIndex)//此循环逐一处理各传感器数据
 	{
 		switch (pSensorHead->byType)
 		{
-		case 1:
-		case 31:
-		case 32:
-		case 33:
-		case 43:
+		case 1:		//RTK,硬件配置错了，type本为20，结果被设置为1了，此处兼容之
+		case 20:	//RTK,今后硬件配置type正确后，依然可以正确解析
 			{
-				SSensorData1	*pSensorData = (SSensorData1*)((char*)pSensorHead + sizeof(SSensorHead));
-				dValue1	= pSensorData->dValue1;
-				dValue2	= pSensorData->dValue2;
-				dValue3	= pSensorData->fValue3;
-				dValue4	= pSensorData->byValue4;
+				SSensorData20	*pSensorData = (SSensorData20 *)((char*)pSensorHead + sizeof(SSensorHead));//得到传感器数据值
+
+				byType = 20;//pSensorHead->byType;//改为20！
+				
+				dValue1	= pSensorData->dX;	//RTK经度值
+				dValue2	= pSensorData->dY;	//RTK纬度值
+				dValue3	= pSensorData->fZ;	//RTK高程值
+				dValue4	= pSensorData->byFix;//RTK FIX指标
 
 				// 临时为了统一做的调整
-				BYTE	byType = pSensorHead->byType + 1;
-
+//				BYTE	byType = pSensorHead->byType + 1;//没必要！
+	
+/*
 				SMysqlRequest	tagRequest = {0};
 				tagRequest.byOpt		= SENSOR_DB_ADD_SENSOR_DATA;
 				tagRequest.uClientID	= m_uUniqueID;
 				tagRequest.uClientIndex	= m_uIndex;
 				tagRequest.byClientType	= DATA_CLIENT;
-
 				pMysqlQuery->PrepareProc("AddSensorData");
 				pMysqlQuery->AddParam(pInfo->wSceneID);
 				pMysqlQuery->AddParam(pInfo->bySlopeType);
@@ -130,106 +132,125 @@ void CDataClient::RecvAddSensorData(const void *pPack, const unsigned int uPackL
 				pMysqlQuery->AddParam((double)pInfo->fLongitude);
 				pMysqlQuery->AddParam((double)pInfo->fLatitude);
 				pMysqlQuery->AddParam(pSensorHead->byID);
-				pMysqlQuery->AddParam(byType);
-				pMysqlQuery->AddParam(dValue1);
-				pMysqlQuery->AddParam(dValue2);
-				pMysqlQuery->AddParam(dValue3);
-				pMysqlQuery->AddParam(dValue4);
+				pMysqlQuery->AddParam(byType);		//按传感器类型20入库！
+				pMysqlQuery->AddParam(dValue1);		//RTK经度值入库
+				pMysqlQuery->AddParam(dValue2);		//RTK纬度值入库
+				pMysqlQuery->AddParam(dValue3);		//RTK高程值入库
+				pMysqlQuery->AddParam(dValue4);		//RTK FIX指标入库
 				pMysqlQuery->EndPrepareProc(&tagRequest, sizeof(tagRequest));
 
 				pMysqlQuery->CallProc();
 				g_pFileLog->WriteLog("SlopeSceneID=%hu SlopeType=%hhu Longitude=%f Latitude=%f SensorSceneID=%hhu SensorType=%hhu Value1=%f Value2=%f Value3=%f Value4=%f\n", pInfo->wSceneID, pInfo->bySlopeType, pInfo->fLongitude, pInfo->fLatitude, pSensorHead->byID, byType, dValue1, dValue2, dValue3, dValue4);
+*/
 			}
 			break;
-		case 2:
+		case 30:	//485_测斜仪_S100
 			{
-				SSensorData2	*pSensorData = (SSensorData2*)((char*)pSensorHead + sizeof(SSensorHead));
-				if (-1 != pSensorData->sData1 && -1 != pSensorData->sData2)
-				{
-					dValue1	= ((double)pSensorData->sData1 / 10000 - 0.95) / 0.95 * 30;
-					dValue2	= ((double)pSensorData->sData2 / 10000 - 0.95) / 0.95 * 30;
-					dValue3	= 0.0;
-					dValue4	= 0.0;
+				double PI=3.141592654;
+				SSensorData30	*pSensorData = (SSensorData30 *)((char*)pSensorHead + sizeof(SSensorHead));
+				byType = pSensorHead->byType;
 
-					// 临时为了统一做的调整
-					BYTE	byType = pSensorHead->byType + 1;
+				short sX=pSensorData->sX;												//X角读数
+				short sY=pSensorData->sY;												//Y角读数
+				dValue1	= asin(((double)sX-1024)/1638)*180/PI;		//X角值(°)
+				dValue2	= asin(((double)sY-1024)/1638)*180/PI;		//Y角值(°)
+				dValue3	= 0.0;
+				dValue4	= 0.0;
 
-					SMysqlRequest	tagRequest = { 0 };
-					tagRequest.byOpt = SENSOR_DB_ADD_SENSOR_DATA;
-					tagRequest.uClientID = m_uUniqueID;
-					tagRequest.uClientIndex = m_uIndex;
-					tagRequest.byClientType = DATA_CLIENT;
 
-					pMysqlQuery->PrepareProc("AddSensorData");
-					pMysqlQuery->AddParam(pInfo->wSceneID);
-					pMysqlQuery->AddParam(pInfo->bySlopeType);
-					pMysqlQuery->AddParam(pInfo->uTime);
-					pMysqlQuery->AddParam((double)pInfo->fLongitude);
-					pMysqlQuery->AddParam((double)pInfo->fLatitude);
-					pMysqlQuery->AddParam(pSensorHead->byID);
-					pMysqlQuery->AddParam(byType);
-					pMysqlQuery->AddParam(dValue1);
-					pMysqlQuery->AddParam(dValue2);
-					pMysqlQuery->AddParam(dValue3);
-					pMysqlQuery->AddParam(dValue4);
-					pMysqlQuery->EndPrepareProc(&tagRequest, sizeof(tagRequest));
-
-					pMysqlQuery->CallProc();
-					g_pFileLog->WriteLog("SlopeSceneID=%hu SlopeType=%hhu Longitude=%f Latitude=%f SensorSceneID=%hhu SensorType=%hhu Value1=%f Value2=%f Value3=%f Value4=%f\n", pInfo->wSceneID, pInfo->bySlopeType, pInfo->fLongitude, pInfo->fLatitude, pSensorHead->byID, byType, dValue1, dValue2, dValue3, dValue4);
-				}
 			}
 			break;
-		case 3:
+		case 31:	//485_雨量计
 			{
-				SSensorData3	*pSensorData = (SSensorData3*)((char*)pSensorHead + sizeof(SSensorHead));
-				if (-1 != pSensorData->sData1 && -1 != pSensorData->sData2)
-				{
-					double	dFi	= (double)pSensorData->sData1 / 10;
-					double	dFo	= 1289;
-					double	dK	= 2.476 / 10000000;
-					dValue1		= dK*(dFi*dFi - dFo*dFo);
-					dValue2		= 0.0;
-					dValue3		= 0.0;
-					dValue4		= 0.0;
+				SSensorData31	*pSensorData = (SSensorData31 *)((char*)pSensorHead + sizeof(SSensorHead));
+				byType = pSensorHead->byType;
 
-					// 临时为了统一做的调整
-					BYTE	byType = pSensorHead->byType + 1;
+				long lYl_Num=pSensorData->lYL_Num;	//雨量计数
+				dValue1		= (double)lYl_Num*0.2;		//雨量值(mm)
+				dValue2		= 0.0;
+				dValue3		= 0.0;
+				dValue4		= 0.0;
 
-					SMysqlRequest	tagRequest = {0};
-					tagRequest.byOpt = SENSOR_DB_ADD_SENSOR_DATA;
-					tagRequest.uClientID = m_uUniqueID;
-					tagRequest.uClientIndex = m_uIndex;
-					tagRequest.byClientType = DATA_CLIENT;
-
-					pMysqlQuery->PrepareProc("AddSensorData");
-					pMysqlQuery->AddParam(pInfo->wSceneID);
-					pMysqlQuery->AddParam(pInfo->bySlopeType);
-					pMysqlQuery->AddParam(pInfo->uTime);
-					pMysqlQuery->AddParam((double)pInfo->fLongitude);
-					pMysqlQuery->AddParam((double)pInfo->fLatitude);
-					pMysqlQuery->AddParam(pSensorHead->byID);
-					pMysqlQuery->AddParam(byType);
-					pMysqlQuery->AddParam(dValue1);
-					pMysqlQuery->AddParam(dValue2);
-					pMysqlQuery->AddParam(dValue3);
-					pMysqlQuery->AddParam(dValue4);
-					pMysqlQuery->EndPrepareProc(&tagRequest, sizeof(tagRequest));
-
-					pMysqlQuery->CallProc();
-					g_pFileLog->WriteLog("SlopeSceneID=%hu SlopeType=%hhu Longitude=%f Latitude=%f SensorSceneID=%hhu SensorType=%hhu Value1=%f Value2=%f Value3=%f Value4=%f\n", pInfo->wSceneID, pInfo->bySlopeType, pInfo->fLongitude, pInfo->fLatitude, pSensorHead->byID, byType, dValue1, dValue2, dValue3, dValue4);
-				}
 			}
 			break;
-		case 4:
+		case 32:	//485_超声液位计
 			{
+				SSensorData32	*pSensorData = (SSensorData32 *)((char*)pSensorHead + sizeof(SSensorHead));
+				byType = pSensorHead->byType;
+				
+				short	dYw_Num	= pSensorData->sL;		//液位读数
+				short	dKj_Num	= pSensorData->sH;		//空距读数
+				dValue1		= (double)dYw_Num/1000;		//液位值(m)
+				dValue2		= (double)dYw_Num/1000;		//空距值(m)
+				dValue3		= 0.0;
+				dValue4		= 0.0;
+
 			}
 			break;
+		case 35:	//485_测斜仪_S3300
+			{
+				SSensorData35	*pSensorData = (SSensorData35 *)((char*)pSensorHead + sizeof(SSensorHead));
+				byType = pSensorHead->byType;
+
+				short sX=pSensorData->sX;					//X角读数
+				short sY=pSensorData->sY;					//Y角读数
+				dValue1	= (double)sX/16384*90;		//X角值(°)
+				dValue2	= (double)sY/16384*90;		//Y角值(°)
+				dValue3	= 0.0;
+				dValue4	= 0.0;
+
+			}
+//		case 40:	//振弦_压力传感器
+//		case 41:	//振弦_锚索计
+//		case 42:	//振弦_锚索计
+//			{
+//				SSensorData4x	*pSensorData = (SSensorData4x *)((char*)pSensorHead + sizeof(SSensorHead));
+//				byType = pSensorHead->byType;
+//				BYTE 	byID = pSensorHead->byID;
+////根据byType和byID找Sensor表中查出振弦参数fK和校准值sf0,//**********************这个函数怎么写，我不会！@叶飞*******************！！！
+////因此所有振弦传感器都一样！！
+//				float K=fK;
+//				short fo=sf0;
+//				short fi=pSensorData->sFi;			//频率读数
+//				float fP=K*(fo*fo-fi*fi);				//算出压强fP
+//	      if (byType == 42)								//如果是渗压计
+//	      		fP=100*fP;									//算出水位来	
+//				dValue1 = (double)fi;
+//				dValue2	= (double)fP;		//频率读数;				//压强值P=K*(fi*fi-fo*fo),系数K 取自Sensor表中对应传感器之P1,频率fo=校准时的fi，在校准时存入Sensor之P2,计算时取出;
+//				dValue3	= 0.0;
+//				dValue4	= 0.0;
+//			}
+//			break;
 		default:
 			g_pFileLog->WriteLog("Length=%hu SensorID=%hhu Invalid SensorType=%hhu\n", pSensorHead->wLength, pSensorHead->byID, pSensorHead->byType+1);
 			break;
 		}
+//数据入库
+		SMysqlRequest	tagRequest = {0};
+		tagRequest.byOpt = SENSOR_DB_ADD_SENSOR_DATA;
+		tagRequest.uClientID = m_uUniqueID;
+		tagRequest.uClientIndex = m_uIndex;
+		tagRequest.byClientType = DATA_CLIENT;
 
-		pSensorHead = (SSensorHead*)((char*)pSensorHead + sizeof(SSensorHead) + pSensorHead->wLength);
+		pMysqlQuery->PrepareProc("AddSensorData");
+		pMysqlQuery->AddParam(pInfo->wSceneID);
+		pMysqlQuery->AddParam(pInfo->bySlopeType);
+		pMysqlQuery->AddParam(pInfo->uTime);
+		pMysqlQuery->AddParam((double)pInfo->fLongitude);
+		pMysqlQuery->AddParam((double)pInfo->fLatitude);
+		pMysqlQuery->AddParam(pSensorHead->byID);
+		pMysqlQuery->AddParam(byType);
+		pMysqlQuery->AddParam(dValue1);
+		pMysqlQuery->AddParam(dValue2);
+		pMysqlQuery->AddParam(dValue3);
+		pMysqlQuery->AddParam(dValue4);
+		pMysqlQuery->EndPrepareProc(&tagRequest, sizeof(tagRequest));
+
+		pMysqlQuery->CallProc();
+//写日志
+		g_pFileLog->WriteLog("SlopeSceneID=%hu SlopeType=%hhu Longitude=%f Latitude=%f SensorSceneID=%hhu SensorType=%hhu Value1=%f Value2=%f Value3=%f Value4=%f\n", pInfo->wSceneID, pInfo->bySlopeType, pInfo->fLongitude, pInfo->fLatitude, pSensorHead->byID, byType, dValue1, dValue2, dValue3, dValue4);
+
+		pSensorHead = (SSensorHead*)((char*)pSensorHead + sizeof(SSensorHead) + pSensorHead->wLength);//指向下一个传感器字头
 	}
 
 	// 正式能插入数据后，将此处删除
--- code/share/header/Protocol/DataServerProtocol.h
+++ code/share/header/Protocol/DataServerProtocol.h
@@ -18,9 +18,9 @@ struct SProtocolHead
 {
 	WORD	wProtocolHead;	// 7E81
 	BYTE	bySlopeType;	// 现场类型ID
-	WORD	wSceneID;		// 现场ID
+	WORD	wSceneID;			// 现场ID
 	BYTE	bySensorCount;	// 本现场传感器数量
-	UINT	uTime;			// 时间
+	UINT	uTime;				// 时间
 	float	fLatitude;		// 现场纬度
 	float	fLongitude;		// 现场经度
 };
@@ -32,30 +32,64 @@ struct SSensorHead
 	BYTE	byID;			// 传感器现场ID
 };
 
-struct SSensorData1
+struct SSensorData20		//RDK=1----应该改为20！
 {
-	double	dValue1;
-	double	dValue2;
-	float	fValue3;
-	BYTE	byValue4;
-	BYTE	byValue5[3];
+	double	dX;			//经度
+	double	dY;			//维度
+	float	fZ;			//高程
+	BYTE	byFix;		//FIX
+	BYTE	byData[1];  //保留
 };
 
-struct SSensorData2
+struct SSensorData30		//485_测斜仪_s100		
 {
-	short	sData1;
-	short	sData2;
-	BYTE	byData[4];
+	short	sX;			//x倾角
+	short	sY;			//y倾角
+	BYTE	byData[4];   //保留
 };
 
-struct SSensorData3
+struct SSensorData31	//485_雨量计
 {
-	short	sData1;
-	short	sData2;
-	BYTE	byData[4];
+	long	lYL_Num;		//雨量lL
+	BYTE	byData[4];  //保留
 };
-// 传感器变长的数据，你看一下怎么定义
 
+struct SSensorData32	//485_超声液位计
+{
+	short	sL;		//液位sL
+	short	sH;		//空距sH
+	BYTE	byData[4];  //保留
+};
+
+struct SSensorData35			//485_测斜仪_s3300
+{
+	short	sX;			//x倾角
+	short	sY;			//y倾角
+	BYTE	byData[4];  //保留
+};
+
+struct SSensorData4x	//振弦_土压力计
+{
+	short	sFi;			//频率读数
+	float	fP;				//压强值P=K*(fi*fi-fo*fo),系数K 取自Sensor表中对应传感器之P1,频率fo=校准时的fi，在校准时存入Sensor之P2,计算时取出;
+	BYTE	byData[2];//保留
+};
+
+/*
+struct SSensorData41	//振弦_锚索计，直接显示频率，
+{
+	short	sFi;		//频率读数f0
+	float	fP;			//载荷值P=K*(fi*fi-fo*fo),系数K 取自Sensor表中对应传感器之P1,频率fo，=校准时的fi，在校准时存入Sensor之P2,计算时取出;
+	BYTE	byData[2];//保留
+};
+
+struct SSensorData42	//振弦_渗压计
+{
+	short	sFi;			//频率读数
+	float	fW;				//水位值W=100*K*(fi*fi-fo*fo)(m),系数K 取自Sensor表中对应传感器之P1,频率fo，=校准时的fi，在校准时存入Sensor之P2,计算时取出;
+	BYTE	byData[2];//保留
+};
+*/
 // CRC校验
 struct SCRCSum
 {
